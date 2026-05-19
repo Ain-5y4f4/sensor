40d5c94a2f7a6713e1d2973160585d5a9ae87691
Ain-5y4f4 <ainfayyadh2@gmail.com>
2026-05-19 10:06:40 +0800

    6.1 done

--- app/src/main/java/com/example/sensor/MainActivity.kt
+++ app/src/main/java/com/example/sensor/MainActivity.kt
@@ -1,47 +1,61 @@
-package com.example.sensor
+package com.example.lab6
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sensor.ui.theme.SensorTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val sensorManager =
+            getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL)
+
         setContent {
-            SensorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+            MaterialTheme {
+
+                Surface {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+
+                        for (sensor in sensorList) {
+
+                            Text(
+                                text =
+                                    "Name: ${sensor.name}\n" +
+                                            "Vendor: ${sensor.vendor}\n" +
+                                            "Version: ${sensor.version}\n" +
+                                            "Maximum Range: ${sensor.maximumRange}\n" +
+                                            "Min Delay: ${sensor.minDelay}\n",
+                                fontSize = 16.sp
+                            )
+
+                            Spacer(modifier = Modifier.height(20.dp))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SensorTheme {
-        Greeting("Android")
     }
 }
